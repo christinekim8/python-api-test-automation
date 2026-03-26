311049cdb80c14dd8f7989e0190b7c84a9e86cf4
christinekim8 <minkim56@gmail.com>
2026-03-26 15:27:50 +1100

    feat: auto-serve allure report via nginx after tests complete

--- run.bat
+++ run.bat
@@ -5,5 +5,6 @@ docker wait woo-test-runner
 docker logs woo-test-runner
 docker compose down
 echo Done!
-echo Opening Allure Report...
-start allure-report\index.html
+echo Opening Allure Report at http://localhost:8888
+docker run --rm -d -p 8888:80 -v "%cd%\allure-report:/usr/share/nginx/html" nginx
+start http://localhost:8888
