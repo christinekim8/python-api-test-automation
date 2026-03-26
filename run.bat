a78716413e85adebd1d5e9b3cefb190f0adab5df
christinekim8 <minkim56@gmail.com>
2026-03-26 14:59:41 +1100

    fix: use docker wait to prevent premature exit on wp-setup completion

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Starting WooCommerce API Automation Tests...
-docker compose up --build --abort-on-container-exit --exit-code-from test-runner
+docker compose up --build --exit-code-from test-runner --abort-on-container-exit
 echo Done!
+echo Open allure-report/index.html in your browser to view the report.
